38da001391d210233b702173b4cba83c77076b8c
Donovan Glover <ultimatedonovan@gmail.com>
2015-05-06 20:00:11 -0400

    add base angular

---
+++ angular/app.js
@@ -0,0 +1,1 @@
+angular.module("EverFrog", ["ngRoute"]).config(function() {})
---
+++ angular/routes.js
@@ -0,0 +1,19 @@
+angular.module("EverFrog").config(["$routeProvider", function($routeProvider) {
+
+	$routeProvider
+		.when("/", {
+			redirectTo: "/home"
+		})
+		.when("/home", {
+			templateUrl: "templates/pages/home/default.htm",
+			controller: "HomeIndexController"
+		})
+		.when("/contact", {
+			templateUrl: "templates/pages/contact/default.htm",
+			controller: "ContactIndexController"
+		})
+		.otherwise({
+			redirectTo: "/"
+		});
+
+}]);
