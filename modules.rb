4618e338c77ad38c2f3e5b5783bc63d6e00caee2
Donovan Glover <ultimatedonovan@gmail.com>
2015-05-06 20:23:04 -0400

    remove uglifier / colorize dependencies

--- modules.rb
+++ modules.rb
@@ -1,15 +1,8 @@
 require "fileutils"
-require "colorize"
-require "uglifier"
 
 require "./modules/build/sass.rb"
 
 require "./modules/files/move.rb"
-
-require "./modules/log/success.rb"
-require "./modules/log/error.rb"
-require "./modules/log/log.rb"
-require "./modules/log/warn.rb"
 
 require "./modules/minify/css.rb"
 
--- modules/combine.rb
+++ modules/combine.rb
@@ -4,7 +4,6 @@ module Combine
 		
 		(given_files).each do |this_file|
 			unless File.file?(this_file)
-				Log.error("'#{this_file}' does not exist!")
 				return false
 			end
 			current_file = open(this_file.to_s)
@@ -16,10 +15,6 @@ module Combine
 			c = open(given_output, "w")
 			c.write(return_this)
 			c.close
-			
-			(given_files).each do |this_file|
-				Log.success("'#{this_file}' successfully compiled to '#{given_output}'.")
-			end
 		end
 		
 		return return_this
