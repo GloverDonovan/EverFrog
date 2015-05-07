ceb8d358a4e94fed3ff4e806d036c2a8a2a25acf
Donovan Glover <ultimatedonovan@gmail.com>
2015-05-06 21:45:27 -0400

    update output files

--- app/assets/main.js
+++ app/assets/main.js
@@ -1,38 +1,41 @@
 "use strict";
 
-;(function ($, window, document, undefined) {
+;(function ($, window, document, undefined) {})(jQuery, window, document);
 
-	var isLateralNavAnimating = false;
-
+/*
+	let isLateralNavAnimating = false;
+	
 	var last = ".selected";
 
-	var doNav = function doNav() {
-		if (!isLateralNavAnimating) {
+	let doNav = () => {
+		if(!isLateralNavAnimating) {
 
-			if ($(undefined).parents(".csstransitions").length > 0) {
+			if($(this).parents('.csstransitions').length > 0) {
 				isLateralNavAnimating = true;
 			}
 
 			$("body").toggleClass("navigation-is-open");
-			$(".cd-navigation-wrapper").one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function () {
+			$(".cd-navigation-wrapper").one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", () => {
 				isLateralNavAnimating = false;
 			});
+
 		}
 	};
 
-	$(document).ready(function () {
+	$(document).ready(() => {
 
-		$(".cd-nav-trigger").on("click", function (e) {
+		$(".cd-nav-trigger").on("click", function(e) {
 			e.preventDefault();
 			doNav();
 		});
 
-		$(".cd-primary-nav a").on("click", function (e) {
+		$(".cd-primary-nav a").on("click", function(e) {
 			e.preventDefault();
 			$(last).removeClass("selected");
 			$(this).addClass("selected");
 			last = this;
 			doNav();
 		});
+
 	});
-})(jQuery, window, document);
+*/
