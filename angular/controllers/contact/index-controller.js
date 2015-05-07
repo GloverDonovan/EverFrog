80e6fd29eae33d35c52382af6df46fa389834861
Donovan Glover <ultimatedonovan@gmail.com>
2015-05-06 21:02:23 -0400

    add angular controllers & directives

---
+++ angular/controllers/contact/index-controller.js
@@ -0,0 +1,3 @@
+angular.module("EverFrog").controller("ContactIndexController", ["$scope", function($scope) {
+	
+}]);
---
+++ angular/controllers/home/index-controller.js
@@ -0,0 +1,3 @@
+angular.module("EverFrog").controller("HomeIndexController", ["$scope", function($scope) {
+	
+}]);
