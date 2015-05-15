fc75371dda5a3e72154fb23794c37f47b862a4f2
GloverDonovan <ultimatedonovan@gmail.com>
2015-05-15 17:56:08 +0000

    upd everfrog form directive js

--- angular/directives/everfrog-form.js
+++ angular/directives/everfrog-form.js
@@ -6,7 +6,19 @@ angular.module("EverFrog").directive("everfrogForm", [function() {
 		restrict: "E",
 		templateUrl: "templates/directives/everfrog-form.htm",
 		controller: function($scope) {},
-		link: function(scope, element, attrs) {}
+		link: function(scope, element, attrs) {
+			
+			$("form").on("submit", function() {
+				
+				$("#wait").fadeIn(200);
+				setTimeout(function() {
+					$("#wait").hide();
+					$("#out").fadeIn(500);
+				}, 2000);
+				
+			});
+			
+		}
 		
 	};
 	
